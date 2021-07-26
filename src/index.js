394e3eeb14bf476ee40af4400b46df72ad5985dd
HuyNQ <quanghuy.tb.vn@gmail.com>
2021-07-26 14:41:42 +0700

    refactor code: create routes, app/controlles

--- src/index.js
+++ src/index.js
@@ -5,13 +5,26 @@ const handlebars  = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 /**
  * set thư mục file tĩnh
  */
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * 2 thằng giúp đóng vai trò như middle ware giữa client và controller
+ * - giúp xử lý dữ liệu và để lấy được qua thằng req.body
+ * - 1 thằng dùng cho post bình thường form html
+ * - thằng dùng cho các thằng gửi qua các thư viện js XMLHttpRequest, fetch, axios,..
+ */
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 //template engine
 const handlebarsConfig = {
@@ -21,12 +34,8 @@ app.engine('hbs', handlebars(handlebarsConfig));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
 
-app.get('/news', (req, res) => {
-    res.render('news');
-})
+// route init
+route(app);
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
